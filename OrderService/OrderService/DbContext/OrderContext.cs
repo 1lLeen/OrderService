894862856b97784588d4b66a01662135cf002597
zhangir003 <zhangir003@mail.ru>
2024-10-09 00:53:36 +0500

    AddDbContext

---
+++ OrderService/OrderService/DbContext/OrderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+namespace OrderService.DbContext
+{
+    public class OrderContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Order> Orders{ get; set; }
+        public OrderContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=orderdb;Username=postgres;Password=jangir");
+        }
+    }
+
+}
