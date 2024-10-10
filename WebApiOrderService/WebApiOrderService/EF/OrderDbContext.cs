3e62651923182a0f91c63fbb65310bd177286a48
zhangir003 <zhangir003@mail.ru>
2024-10-11 01:08:36 +0500

    AddContext

---
+++ WebApiOrderService/WebApiOrderService/EF/OrderDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WebApiOrderService.Models.OrderModels;
+
+namespace WebApiOrderService.EF
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options) { }
+
+        public DbSet<Order> Orders{ get; set; }
+
+    }
+}
--- WebApiOrderService/WebApiOrderService/Program.cs
+++ WebApiOrderService/WebApiOrderService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiOrderService.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,5 +10,8 @@ var app = builder.Build();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 // Configure the HTTP request pipeline.
 
+builder.Services.AddDbContextPool<OrderDbContext>(opt =>
+    opt.UseNpgsql(
+        builder.Configuration.GetConnectionString(connectionString)));
 app.UseHttpsRedirection();
 app.Run();
