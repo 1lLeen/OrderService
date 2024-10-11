056fa8eb921649e3f415d7405bf0ce0519083c51
zhangir003 <zhangir003@mail.ru>
2024-10-11 11:50:04 +0500

    AddDtoModel

---
+++ WebApiOrderService/WebApiOrderService/Models/DtoOrders/DtoOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiOrderService.Models.DtoOrders
+{
+    public class DtoOrder
+    {
+        public int OrderId { get; set; }
+        public string OrderName { get; set; }
+        public string OrderDescription { get; set; }
+        public int OrderPrice { get; set; }
+
+    }
+}
