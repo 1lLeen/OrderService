9687bd5e05348813c5e42a558a1a76a39fcf587c
zhangir003 <zhangir003@mail.ru>
2024-10-11 11:44:09 +0500

    AddRepository

---
+++ WebApiOrderService/WebApiOrderService/Repositories/InterfacesRepositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApiOrderService.Models.OrderModels;
+
+namespace WebApiOrderService.Repositories.InterfacesRepositories
+{
+    public interface IOrderRepository
+    {
+        List<Order> GetAllOrders();
+        Order GetOrder(int id);
+        void PostOrder(Order order);
+        void PutOrder(Order order);
+        void DeleteOrder(int id);
+
+    }
+}
---
+++ WebApiOrderService/WebApiOrderService/Repositories/OrderRepository.cs
@@ -0,0 +1,63 @@
+﻿using WebApiOrderService.EF;
+using WebApiOrderService.Models.OrderModels;
+using WebApiOrderService.Repositories.InterfacesRepositories;
+
+namespace WebApiOrderService.Repositories
+{
+    public class OrderRepository:IOrderRepository
+    {
+        public OrderDbContext _context;
+        public OrderRepository(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteOrder(int id)
+        {
+            if(id > 0)
+            {
+                var order = GetOrder(id);
+                if (order != null)
+                {
+                    _context.Orders.Remove(order);
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public Order GetOrder(int id)
+        {
+            return _context.Orders.FirstOrDefault(o => o.Id == id);
+        }
+
+        public void PostOrder(Order order)
+        {
+            if(order != null)
+            {
+                if (!ExistsOrder(order))
+                {
+                    _context.Orders.Add(order);
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        public void PutOrder(Order order)
+        {
+            if (ExistsOrder(order)) 
+            {
+                _context.Orders.Entry(order);
+                _context.SaveChanges();
+            }
+        }
+        private bool ExistsOrder(Order order)
+        {
+            return _context.Orders.Any(o => o.Id == order.Id);
+        }
+    }
+}
