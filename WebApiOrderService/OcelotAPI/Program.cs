13a2025e6586c1ee4a41bcbd28ecf314af732a33
zhangir003 <zhangir003@mail.ru>
2024-10-24 17:43:27 +0500

    IncludeOcelot

--- WebApiOrderService/OcelotAPI/Program.cs
+++ WebApiOrderService/OcelotAPI/Program.cs
@@ -1,12 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json", optional:false, reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+app.MapControllers();
+
+await app.UseOcelot();
+
 
 app.Run();
 
